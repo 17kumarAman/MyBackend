c9db6e61d78d5830b0f0810583dec449f38656e5
Manishrajwar <manishsinghrajwar80@gmail.com>
2024-10-04 20:43:56 +0530

    permission done

--- models/User/User.js
+++ models/User/User.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
     type:String , 
     default:"15"
   } , 
+  showTasksDetailPermission:{
+    type: Boolean , 
+    default: false , 
+  } , 
   userAllowCrtPermission:{
     type: Boolean , 
     default:false , 
